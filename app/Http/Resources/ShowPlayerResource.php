f1d2e9dd9258ef5d2b2deffcc03f0d0d146b7e13
Francis <90559251+francisape1997@users.noreply.github.com>
2022-04-27 00:50:07 +0800

    Add player CRUD api resources

---
+++ app/Http/Resources/ShowPlayerResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShowPlayerResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return
+        [
+            'id'            => $this->id,
+            'first_name'    => $this->first_name,
+            'second_name'   => $this->second_name,
+            'last_name'     => $this->last_name,
+            'date_of_birth' => $this->date_of_birth,
+            'height'        => $this->height,
+            'weight'        => $this->weight,
+            'form'          => $this->form,
+            'influence'     => $this->influence,
+            'creativity'    => $this->creativity,
+            'threat'        => $this->threat,
+            'ict_index'     => $this->ictIndex,
+            'metrics'       => config('player.metric'),
+        ];
+    }
+}
