bf677f56cbe3aed336ee842af0ef2b15e3e37335
Francis <90559251+francisape1997@users.noreply.github.com>
2022-04-27 14:05:05 +0800

    Remove import player route

--- routes/api/player.php
+++ routes/api/player.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayerController;
 
 Route::resource('player', PlayerController::class)->except('create');
-Route::post('player/import', [PlayerController::class, 'importPlayers']);
