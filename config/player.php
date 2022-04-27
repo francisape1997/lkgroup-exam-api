50008b8ec92b5f07794e6920f7a776f77ddd29ff
Francis <90559251+francisape1997@users.noreply.github.com>
2022-04-27 14:02:27 +0800

    Add importer prop to player config

--- config/player.php
+++ config/player.php
@@ -5,5 +5,10 @@ return
     'metric' => [
         'height' => 'cm',
         'weight' => 'lb'
-    ]
+    ],
+
+    'importer' => [
+        'data_source' => 'factory',
+        'minimum' => 100,
+    ],
 ];
